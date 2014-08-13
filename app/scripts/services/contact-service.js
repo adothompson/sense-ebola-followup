1b98fb3379dd12204600de70f03bb50915a0d7c3
jofomah <jideobi.ofomah@ehealthnigeria.org>
2014-08-13 18:46:27 +0100

    adds sync status to report

--- app/scripts/services/contact-service.js
+++ app/scripts/services/contact-service.js
@@ -1,39 +1,45 @@
 'use strict';
 
 angular.module('lmisChromeApp')
-  .service('contactService', function ($q, storageService) {
+  .service('contactService', function($q, storageService, syncService) {
 
     this.CONTACT_DB = 'sense_contacts';
 
-    this.save = function(contact){
+    this.save = function(contact) {
       return storageService.save(this.CONTACT_DB, contact);
     };
 
-    this.get =  function(id){
-      return storageService.get(this.CONTACT_DB, id);
+    this.get = function(id) {
+      return storageService.get(this.CONTACT_DB, id)
+        .then(function(c) {
+          return syncService.getSyncStatus(c);
+        });
     };
 
-    this.all = function(){
-      return storageService.all(this.CONTACT_DB);
+    this.all = function() {
+      return storageService.all(this.CONTACT_DB)
+        .then(function(contacts){
+          return syncService.addSyncStatus(contacts);
+        });
     };
 
-    this.contactGroupedByName = function(){
+    this.contactGroupedByName = function() {
       var fullNames = {};
       var contact;
       var fullName;
       return this.all()
-        .then(function(contacts){
-          for(var i in contacts){
-            contact  = contacts[i];
+        .then(function(contacts) {
+          for (var i in contacts) {
+            contact = contacts[i];
             fullName = [contact.Surname, contact.OtherNames].join(' ').toLowerCase();
             fullNames[fullName] = contact;
           }
           return fullNames;
         })
-        .catch(function(reason){
+        .catch(function(reason) {
           console.error(reason);
           return fullNames;
         });
     };
 
-});
+  });
